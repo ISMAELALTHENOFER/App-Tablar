01d576a3952f223cea71d69a29866fe665905b57
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-10 08:38:00 -0300

    se agrega la ruta de productos

--- App-Tablar/routes/web.php
+++ App-Tablar/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,4 @@ Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::resource('/productos', ProductoController::class);
