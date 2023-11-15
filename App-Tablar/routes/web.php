5335a7113f18a81eff5129efb99bf56f8a2f5579
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-15 10:23:50 -0300

    Update web.php

--- App-Tablar/routes/web.php
+++ App-Tablar/routes/web.php
@@ -22,8 +22,4 @@ Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::resource('/productos', ProductoController::class);
